da8e721fc92175727dbe193779b79d71a3e2cffa
flaviovitale02 <flaviovitale73@gmail.com>
2023-05-19 10:48:05 +0200

    aggiunto codice più corretto snack 1

--- snack 1/script.js
+++ snack 1/script.js
@@ -7,3 +7,11 @@ for(let i = 0 ; i < 10 ; i++){
     let finalNumber = array.reduce((partialSum, a) => partialSum + a, 0);
     console.log(finalNumber)
 }
+
+let sum = 0
+
+for(let i = 0 ; i < 10 ; i++){
+    let userNumber = parseInt(prompt('inserisci un numero'))
+    sum += userNumber
+}
+console.log(sum)
